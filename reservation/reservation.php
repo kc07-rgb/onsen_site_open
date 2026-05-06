64649fbd5166cf9077448f3d20cb9180f1b79f0a
kc07-rgd <pokemonn0724@gmail.com>
2026-05-07 06:37:40 +0900

    Add reservation

---
+++ reservation/reservation.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>鳥沢温泉 | 予約</title>
+</head>
+<body>
+    <div class="day_people">
+        <label for="checkin">チェックイン</label>
+        <input type="date" name="checkin" id="checkin">
+    </div>
+</body>
+
+<script>
+    
+</script>
+</html>
