2a2295ca80fe31a5fb6071ca5d006851c7a632fb
kc07-rgd <pokemonn0724@gmail.com>
2026-06-11 22:05:29 +0900

    reg

---
+++ admin/news_category.php
@@ -0,0 +1,15 @@
+<?php
+try {
+    $pdo = new PDO(
+        'mysql:host=localhost;dbname=onsen_hotel_site',
+        "root",
+        "",
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    exit("DB接続エラー");
+}
+
+
+?>
