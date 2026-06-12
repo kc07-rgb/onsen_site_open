826dfc207b379042865084a00ea6a19acc4b750b
kc07-rgd <pokemonn0724@gmail.com>
2026-06-12 20:11:29 +0900

    news after

--- news/news_list.php
+++ news/news_list.php
@@ -93,22 +93,34 @@ $pdo = null;
 </head>
 
 <body>
-<header id="header">
-    <div id="header-top">
-        <a href="../header.php" class="header-logo">鳥沢温泉</a>
+    <?php if (isset($_SESSION["user_id"])): ?>
+        <div class="admin_header">
+            <p>鳥沢温泉/管理画面</p>
+            <nav>
+                <ul>
+                    <li><a href="./news_form.php">ニュース投稿</a></li>
+                    <li><a href="/温泉/users/logout.php">ログアウト</a></li>
+                </ul>
+            </nav>
+        </div>
+    <?php endif; ?>
 
-        <nav class="header-top-nav">
-            <ul class="header-top-list">
-                <li><a href="/温泉/news/news_list.php">お知らせ</a></li>
-                <li><a href="">よくあるご質問</a></li>
-                <li><a href="/温泉/contact/contact.html">お問い合わせ</a></li>
-                <li><a href="/温泉/reservation/reservation.php">ご予約</a></li>
-                <li><a href=""><i class='bx  bx-camera-alt' style='color:#fff'></i> </a></li>
-            </ul>
-        </nav>
-    </div>
+    <header id="header">
+        <div id="header-top">
+            <a href="../header.php" class="header-logo">鳥沢温泉</a>
 
-        <div id="header-vew">
+            <nav class="header-top-nav">
+                <ul class="header-top-list">
+                    <li><a href="/温泉/news/news_list.php">お知らせ</a></li>
+                    <li><a href="">よくあるご質問</a></li>
+                    <li><a href="/温泉/contact/contact.html">お問い合わせ</a></li>
+                    <li><a href="/温泉/reservation/reservation.php">ご予約</a></li>
+                    <li><a href=""><i class='bx  bx-camera-alt' style='color:#fff'></i> </a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div id="header-vew" class="<?= isset($_SESSION["user_id"]) ? "admin" : "" ?>">
             <a href="../header.php" class="header-vew-logo">鳥沢温泉</a>
             <nav class="header-nav">
                 <ul class="header-nav-list">
@@ -119,36 +131,37 @@ $pdo = null;
                 </ul>
             </nav>
         </div>
-</header>
 
-        <div id="page">
-            <div class="page_tatile">
-                <h2 class="jp">お知らせ</h2>
-                <p class="em">News</p>
-                <p class="description">鳥沢温泉からみなさまへの<br>
-                    大切なお知らせをお届けいたします。</p>
-            </div>
-            <!--wave-->
-            <div class="pc_wave custom-shape-divider-bottom-1779276235">
-                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                    <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
-                </svg>
-            </div>
-            <div class="smartphon_wave">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
-                    <path fill="#f4f0e8" d="M0,192L60,202.7C120,213,240,235,360,218.7C480,203,600,149,720,149.3C840,149,960,203,1080,208C1200,213,1320,171,1380,149.3L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
-                </svg>
-            </div>
-        </div>
     </header>
 
-<!--hamburger-->
+    <div id="page">
+        <div class="page_tatile">
+            <h2 class="jp">お知らせ</h2>
+            <p class="em">News</p>
+            <p class="description">鳥沢温泉からみなさまへの<br>
+                大切なお知らせをお届けいたします。</p>
+        </div>
+        <!--wave-->
+        <div class="pc_wave custom-shape-divider-bottom-1779276235">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+            </svg>
+        </div>
+        <div class="smartphon_wave">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
+                <path fill="#f4f0e8" d="M0,192L60,202.7C120,213,240,235,360,218.7C480,203,600,149,720,149.3C840,149,960,203,1080,208C1200,213,1320,171,1380,149.3L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+            </svg>
+        </div>
+    </div>
+    </header>
+
+    <!--hamburger-->
     <div class="hamburger">
         <div class="top"></div>
         <div class="middle"></div>
         <div class="bottom"></div>
     </div>
-    
+
     <div class="hamburger-container">
         <h2><a href="../header.php">鳥沢温泉</a></h2>
         <nav class="hamburger-nav">
@@ -167,18 +180,6 @@ $pdo = null;
             </ul>
         </nav>
     </div>
-
-
-    <div class="nav">
-        <nav>
-            <a href="../header.php">鳥沢温泉</a>
-            <a href="./news_form.php">ニュースフォーム</a>
-            <a href="/温泉/users/user_ registration.php">ユーザー登録</a>
-            <a href="../users/login.php">ログイン</a>
-            <a href="/温泉/users/logout.php">ログアウト</a>
-        </nav>
-    </div>
-
 
 
     <div class="news_list">
@@ -282,6 +283,7 @@ $pdo = null;
         </div>
     </footer>
 
-<script src="../app.js"></script>
+    <script src="../app.js"></script>
 </body>
+
 </html>
