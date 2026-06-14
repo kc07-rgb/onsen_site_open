f5e9506cef856deded2cf2b1331bf90d238b2267
kc07-rgd <pokemonn0724@gmail.com>
2026-06-14 18:12:12 +0900

    Update dbname

--- reservation/confirm.php
+++ reservation/confirm.php
@@ -7,9 +7,9 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
 
 try {
     $pdo = new PDO(
-        'mysql:host=localhost;dbname=onsen_hotel_site',
-        "root",
-        "",
+        'mysql:host=localhost;dbname=konsent_onsen',
+        "konsent_onsen",
+        "Tika0724",
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
     );
 } catch (PDOException $e) {
